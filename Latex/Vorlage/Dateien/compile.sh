5887b2ac8ad1bebc9a982a43c772821695ef67bd
Daniel Freire Mendes <danielfmhaw@gmail.com>
2024-12-13 19:37:29 +0100

    reorganized project structure

--- Latex/Vorlage/Dateien/compile.sh
+++ Latex/Vorlage/Dateien/compile.sh
@@ -7,13 +7,8 @@ biber "$filename"
 lualatex "$filename.tex"
 lualatex "$filename.tex"
 
-output_dir="./Out"
-if [ ! -d "$output_dir" ]; then
-  mkdir "$output_dir"
-fi
-
-mv "$filename.pdf" "$output_dir/"
+mv "$filename.pdf" "../VorlageBA_gen.pdf"
 rm "$filename".aux "$filename".log "$filename".bcf "$filename".bbl "$filename".blg "$filename".run.xml
 rm "$filename".lof "$filename".lot "$filename".out "$filename".toc
-rm appendix.aux benchmarks.aux indexing.aux chap1.aux chap2.aux chap3.aux title.aux toc.aux execution.aux
+rm appendix.aux chap1.aux chap2.aux chap3.aux title.aux toc.aux
 echo "Finished"
