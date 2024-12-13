cf0198ea91009d2dd4207f35ed6fd2a06e8c49ee
Daniel Freire Mendes <danielfmhaw@gmail.com>
2024-12-13 19:37:29 +0100

    reorganized project structure

---
+++ Latex/Arbeit/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filename="VorlageBA"
+
+lualatex "$filename.tex"
+biber "$filename"
+lualatex "$filename.tex"
+lualatex "$filename.tex"
+
+output_dir="./Out"
+if [ ! -d "$output_dir" ]; then
+  mkdir "$output_dir"
+fi
+
+mv "$filename.pdf" "$output_dir/"
+rm "$filename".aux "$filename".log "$filename".bcf "$filename".bbl "$filename".blg "$filename".run.xml
+rm "$filename".lof "$filename".lot "$filename".out "$filename".toc
+rm appendix.aux benchmarks.aux execution.aux indexing.aux title.aux toc.aux
+echo "Finished"
