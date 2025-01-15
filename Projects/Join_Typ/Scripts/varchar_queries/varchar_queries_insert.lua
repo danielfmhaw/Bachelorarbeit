12d352ff134686a10417a16da76fb17667be5199
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-01-15 09:42:38 +0100

    fixes varchar errors

--- Projects/Join_Typ/Scripts/varchar_queries/varchar_queries_insert.lua
+++ Projects/Join_Typ/Scripts/varchar_queries/varchar_queries_insert.lua
@@ -1,6 +1,7 @@
 local length = tonumber(os.getenv("LENGTH")) or 0
 
 local num_rows = 700
+local bestellungProKunde = 3
 
 -- Function to generate a random string of a given length
 local function randomString(length)
@@ -14,13 +15,15 @@ local function randomString(length)
 end
 
 function delete_data()
-    local delete_kunden_query = "DELETE FROM KUNDEN;"
+    local delete_bestellung_query = "DELETE FROM BESTELLUNGMITVARCHAR;"
+    local delete_kunden_query = "DELETE FROM KUNDENMITVARCHAR;"
     db_query("START TRANSACTION")
+    db_query(delete_bestellung_query)
     db_query(delete_kunden_query)
     db_query("COMMIT")
 end
 
--- Function to insert randomized data into KUNDEN
+-- Function to insert randomized data into KUNDENMITVARCHAR and BESTELLUNGMITVARCHAR
 function insert_data()
     delete_data()
     for i = 1, num_rows do
@@ -33,14 +36,30 @@ function insert_data()
         local email = string.format("customer%d@example.com", i)
         local telefonnummer = string.format("+49157%07d", math.random(1000000, 9999999))
 
-        -- Insert into KUNDEN, ignoring duplicates
+        -- Insert into KUNDENMITVARCHAR, ignoring duplicates
         local kunden_query = string.format([[
-            INSERT IGNORE INTO KUNDEN
+            INSERT IGNORE INTO KUNDENMITVARCHAR
             (NAME, GEBURTSTAG, ADRESSE, STADT, POSTLEITZAHL, LAND, EMAIL, TELEFONNUMMER)
             VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
         ]], name, geburtstag, adresse, stadt, postleitzahl, land, email, telefonnummer)
 
         db_query(kunden_query)
+
+        for j = 1, bestellungProKunde do
+            local bestellung_id = (i - 1) * bestellungProKunde + j
+            local bestelldatum = string.format("2024-%02d-%02d", math.random(1, 12), math.random(1, 28))
+            local artikel_id = math.random(1, 1000)
+            local umsatz = math.random(100, 1000)
+
+            -- Insert into BESTELLUNGMITVARCHAR, ignoring duplicates
+            local bestellung_query = string.format([[
+              INSERT IGNORE INTO BESTELLUNGMITVARCHAR
+              (BESTELLUNG_ID, BESTELLDATUM, ARTIKEL_ID, FK_KUNDEN, UMSATZ)
+              VALUES (%d,'%s', %d, '%s', %d);
+            ]],bestellung_id, bestelldatum, artikel_id, name, umsatz)
+
+            db_query(bestellung_query)
+        end
     end
 end
 
