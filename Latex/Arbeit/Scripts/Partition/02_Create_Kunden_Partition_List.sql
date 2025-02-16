57db97344fa33aa9e666c904407974e9fc6da1be
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-02-16 16:39:20 +0100

    updates latex-files, adds script and pngs for partition

---
+++ Latex/Arbeit/Scripts/Partition/02_Create_Kunden_Partition_List.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS KUNDEN (
+    KUNDEN_ID     INT NOT NULL,
+    LAND          VARCHAR(100) NOT NULL,
+    -- other attributes
+    PRIMARY KEY (KUNDEN_ID, LAND)
+    )
+PARTITION BY LIST COLUMNS(LAND) (
+    PARTITION p_china VALUES IN ('China'),
+    PARTITION p_india VALUES IN ('India'),
+    PARTITION p_united_states VALUES IN ('United States'),
+    -- other partitions
+    PARTITION p_thailand VALUES IN ('Thailand'),
+    PARTITION p_other VALUES IN ('Other')
+);
