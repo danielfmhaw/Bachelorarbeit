9d775817cd66dc58c75f39f1c6cfbfb81832af95
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-03-05 20:54:51 +0100

    updates tools.tex and related scripts and pngs

--- Latex/Arbeit/Scripts/Grundlagen/07_Orchestrator_Command.sh
+++ Latex/Arbeit/Scripts/Grundlagen/07_Orchestrator_Command.sh
@@ -1,5 +1,11 @@
 ./sysbench_script.sh \
   -out "YOUR_PATH_TO_DIRECTORY/Output" \
-  -var '{"length":[1,64]}' \
-  -scripts:"YOUR_PATH_TO_DIRECTORY/int_queries" \
-  "YOUR_PATH_TO_DIRECTORY/varchar_queries:length"
+  -var '{"length":[4, 16]}' \
+  -scripts '{
+    "YOUR_PATH_TO_DIRECTORY/Scripts/varchar_queries": {
+      "vars": "length"
+    },
+    "YOUR_PATH_TO_DIRECTORY/Scripts/int_queries": {
+      "vars": "length"
+    }
+  }'
--- Latex/Arbeit/Scripts/Grundlagen/08_Main_Loop.sh
+++ Latex/Arbeit/Scripts/Grundlagen/08_Main_Loop.sh
@@ -1,25 +1,30 @@
-# Main benchmark loop
-for INFO in "${QUERY_INFO[@]}"; do
-  # Definition of the variables comes here
+for SCRIPT_PATH in $SCRIPT_DIRS; do
+  DBMS=$(echo "$SCRIPTS" | jq -r --arg key "$SCRIPT_PATH" '.[$key].db // ["mysql"]')
+  SELECT_QUERIES=$(echo "$SCRIPTS" | jq -r --arg key "$SCRIPT_PATH" '.[$key].selects')
+  for DB in $(echo "$DBMS" | jq -r '.[]'); do
+    prepare_variables "$SCRIPT_PATH" "$DB"
+    DB_INFO="$( [ "$DBMS_COUNT" -ne 1 ] && echo "${DB}" )"
+    if [[ -n "$EXPORTED_VARS" ]]; then
+        IFS=',' read -r -a KEYS <<< "$EXPORTED_VARS"
 
-  if [[ -n "$EXPORTED_VARS" ]]; then
-    IFS=';' read -r -a KEYS <<< "$EXPORTED_VARS"
-    combinations=$(generate_combinations "" "${KEYS[@]}")
-    # Process each combination
-    while IFS=',' read -r combination; do
-        IFS=',' read -ra key_value_pairs <<< "$combination"
-        for pair in "${key_value_pairs[@]}"; do
-          export "$(echo "${pair%%=*}" | tr '[:lower:]' '[:upper:]')=${pair#*=}"
-        done
-        COMBINATION_NAME=$(echo "$combination" | sed -E 's/(^|,)num_rows=[^,]*//g;s/^,//;s/,$//' | tr ',' '_' | tr '=' '_')
-        LOG_DIR_KEY_VALUE="$LOG_DIR/$COMBINATION_NAME"
-        mkdir -p "$LOG_DIR_KEY_VALUE"
-
-        process_script_benchmark "$QUERY_PATH" "$LOG_DIR_KEY_VALUE" "$INSERT_SCRIPT" "$SELECT_SCRIPT" "$COMBINATION_NAME"
-    done <<< "$combinations"
-  else
-    # Process normally when no keys specified
-    mkdir -p "$LOG_DIR"
-    process_script_benchmark "$QUERY_PATH" "$LOG_DIR" "$INSERT_SCRIPT" "$SELECT_SCRIPT"
-  fi
+        COMBINATIONS=$(generate_combinations "" "${KEYS[@]}")
+        while IFS=',' read -r combination; do
+            # Export key-value pairs for the current combination
+            IFS=',' read -ra key_value_pairs <<< "$combination"
+            for pair in "${key_value_pairs[@]}"; do
+              export "$(echo "${pair%%=*}" | tr '[:lower:]' '[:upper:]')=${pair#*=}"
+            done
+            COMBINATION_NAME=$(echo "$combination" | sed -E 's/(^|,)num_rows=[^,]*//g;s/^,//;s/,$//' | tr ',' '_' | tr '=' '_')
+            LOG_DIR_COMBINATION="$LOG_DIR/$COMBINATION_NAME"
+            process_script_benchmark "$DB_INFO" "$SCRIPT_PATH" "$LOG_DIR_COMBINATION" "$INSERT_SCRIPT" "$SELECT_SCRIPT" "$COMBINATION_NAME"
+        done <<< "$COMBINATIONS"
+    else
+      process_script_benchmark "$DB_INFO" "$SCRIPT_PATH" "$LOG_DIR" "$INSERT_SCRIPT" "$SELECT_SCRIPT"
+    fi
+    eval $(jq -r --arg env "$DB" '.[$env] | to_entries | .[] | "unset " + .key' "$ABS_PATH/envs.json")
+  done
 done
+# Combine csv files during runtime and end statistics and generate plots
+python3 "$PYTHON_PATH/generateCombinedCSV.py" "$STATISTICS_FILE_TEMP" "$STATISTICS_FILE" --select_columns "$STATS_SELECT_COLUMNS" --insert_columns "$STATS_INSERT_COLUMNS" --prefixes "$PREFIXES"
+python3 "$PYTHON_PATH/generateCombinedCSV.py" "$RUNTIME_FILE_TEMP" "$RUNTIME_FILE" --select_columns "$RUNTIME_SELECT_COLUMNS" --insert_columns "$RUNTIME_INSERT_COLUMNS" --prefixes "$PREFIXES"
+python3 "$PYTHON_PATH/generatePlot.py" "$RUNTIME_FILE" "$STATISTICS_FILE"
