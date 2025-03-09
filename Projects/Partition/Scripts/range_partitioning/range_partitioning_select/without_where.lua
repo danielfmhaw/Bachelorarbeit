847d891aeb0ebd6f09a38a486ab3086ce0f9df9f
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-03-09 18:25:52 +0100

    updates partition.tex, related scripts and pngs

---
+++ Projects/Partition/Scripts/range_partitioning/range_partitioning_select/without_where.lua
@@ -0,0 +1,24 @@
+local con = sysbench.sql.driver():connect()
+package.path = package.path .. ";" .. debug.getinfo(1).source:match("@(.*)"):match("(.*/)") .. "../../../../../Tools/Lua/?.lua"
+local utils = require("utils")
+local explain_executed = false
+
+function select_failing_pruning()
+    local failing_pruning_query = [[
+        SELECT *
+        FROM KUNDEN k
+        JOIN BESTELLUNG b ON k.KUNDEN_ID = b.FK_KUNDEN
+    ]];
+
+    if not explain_executed then
+        utils.print_results(con, "EXPLAIN " .. failing_pruning_query)
+        utils.print_results(con, (failing_pruning_query:gsub("%*", "COUNT(*)")))
+        explain_executed = true
+    end
+
+    con:query(failing_pruning_query)
+end
+
+function event()
+    select_failing_pruning()
+end
