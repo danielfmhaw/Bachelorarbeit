c499d7c0b170f0ad55ae6c9b8889a9a0c40789b9
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-03-11 14:50:09 +0100

    adds sources for all scripts and lots of improvements

--- Latex/Arbeit/Scripts/Partition/02_Create_Kunden_Partition_List.sql
+++ Latex/Arbeit/Scripts/Partition/02_Create_Kunden_Partition_List.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS KUNDEN (
     LAND          VARCHAR(100) NOT NULL,
     -- other attributes
     PRIMARY KEY (KUNDEN_ID, LAND)
-    )
-PARTITION BY LIST COLUMNS(LAND) (
+) PARTITION BY LIST COLUMNS(LAND) (
     PARTITION p_china VALUES IN ('China'),
     PARTITION p_india VALUES IN ('India'),
     PARTITION p_united_states VALUES IN ('United States'),
