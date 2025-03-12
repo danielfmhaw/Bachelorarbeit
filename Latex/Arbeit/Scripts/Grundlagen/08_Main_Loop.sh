3e73270be141c5b4064a79957ae603bdc65a3d78
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-03-12 17:09:02 +0100

    changes during reading

--- Latex/Arbeit/Scripts/Grundlagen/08_Main_Loop.sh
+++ Latex/Arbeit/Scripts/Grundlagen/08_Main_Loop.sh
@@ -3,7 +3,7 @@ for SCRIPT_PATH in $SCRIPT_DIRS; do
   SELECT_QUERIES=$(echo "$SCRIPTS" | jq -r --arg key "$SCRIPT_PATH" '.[$key].selects')
   for DB in $(echo "$DBMS" | jq -r '.[]'); do
     prepare_variables "$SCRIPT_PATH" "$DB"
-    DB_INFO="$( [ "$DBMS_COUNT" -ne 1 ] && echo "${DB}" )"
+    DB_INFO="$( [ "$DBMS_COUNT" -ne 1 ] && echo "${CUSTOM_DB_NAME}" )"
     if [[ -n "$EXPORTED_VARS" ]]; then
         IFS=',' read -r -a KEYS <<< "$EXPORTED_VARS"
 
@@ -24,6 +24,7 @@ for SCRIPT_PATH in $SCRIPT_DIRS; do
     eval $(jq -r --arg env "$DB" '.[$env] | to_entries | .[] | "unset " + .key' "$ABS_PATH/envs.json")
   done
 done
+
 # Combine csv files during runtime and end statistics and generate plots
 python3 "$PYTHON_PATH/generateCombinedCSV.py" "$STATISTICS_FILE_TEMP" "$STATISTICS_FILE" --select_columns "$STATS_SELECT_COLUMNS" --insert_columns "$STATS_INSERT_COLUMNS" --prefixes "$PREFIXES"
 python3 "$PYTHON_PATH/generateCombinedCSV.py" "$RUNTIME_FILE_TEMP" "$RUNTIME_FILE" --select_columns "$RUNTIME_SELECT_COLUMNS" --insert_columns "$RUNTIME_INSERT_COLUMNS" --prefixes "$PREFIXES"
