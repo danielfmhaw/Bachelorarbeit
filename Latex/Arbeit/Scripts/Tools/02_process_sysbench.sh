15e6c6b96e45ffcd9789bdb8c4776dbcb9bc6b33
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-03-12 17:09:02 +0100

    changes during reading

--- Latex/Arbeit/Scripts/Tools/02_process_sysbench.sh
+++ Latex/Arbeit/Scripts/Tools/02_process_sysbench.sh
@@ -1,11 +1,6 @@
-# Define necessary variables: DB_HOST, DB_USER, DB_PASS, DB_NAME, TABLES, TABLE_SIZE, DURATION,
-SYSBENCH_OPTS="--db-driver=mysql --mysql-host=$DB_HOST --mysql-user=$DB_USER --mysql-password=$DB_PASS --mysql-db=$DB_NAME --tables=$TABLES --table-size=$TABLE_SIZE"
+# Zunächst folgendes definieren : DB_HOST, DB_USER, DB_PASS, DB_NAME, TABLES, TABLE_SIZE, DURATION
+SYSBENCH_CONFIG="--db-driver=mysql --mysql-host=$DB_HOST --mysql-user=$DB_USER --mysql-password=$DB_PASS --mysql-db=$DB_NAME --tables=$TABLES --table-size=$TABLE_SIZE"
 
-# Prepare the database
-sysbench oltp_read_write $SYSBENCH_OPTS prepare >> "$RAW_RESULTS_FILE" 2>&1
-
-# Run the benchmark
-sysbench oltp_read_write $SYSBENCH_OPTS --time=$DURATION --threads=1 --report-interval=1 run >> "$RAW_RESULTS_FILE" 2>&1
-
-# Cleanup the database
-sysbench oltp_read_write $SYSBENCH_OPTS cleanup >> "$RAW_RESULTS_FILE" 2>&1
+sysbench oltp_read_write $SYSBENCH_CONFIG prepare
+sysbench oltp_read_write $SYSBENCH_CONFIG --time=$DURATION --threads=1 --report-interval=1 run
+sysbench oltp_read_write $SYSBENCH_CONFIG cleanup
--- Latex/Arbeit/Scripts/Tools/04_graph_generation.sh
+++ Latex/Arbeit/Scripts/Tools/04_graph_generation.sh
@@ -1,11 +1,9 @@
 OUTPUT_FILE="$OUTPUT_DIR/sysbench_output.csv"
 
 # Gnuplot
-GNUPLOT_SCRIPT="/Users/danielmendes/Desktop/Bachelorarbeit/Repo/plot_sysbench.gp"
+GNUPLOT_SCRIPT="YOUR_PATH_TO_PROJECT/plot_sysbench.gp"
 gnuplot $GNUPLOT_SCRIPT
-echo "Plots generated with gnuplot"
 
 # Python with Library Pandas
-PYTHON_SCRIPT="/Users/danielmendes/Desktop/Bachelorarbeit/Repo/generatePlot.py"
+PYTHON_SCRIPT="YOUR_PATH_TO_PROJECT/generatePlot.py"
 python3 "$PYTHON_SCRIPT" "$OUTPUT_FILE"
-echo "Plots generated with pandas"
